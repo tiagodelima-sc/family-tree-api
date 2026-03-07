0cc329912f177db989a138739c192bc493f848f9
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2026-03-07 14:57:38 -0300

    feat: desenvolvido nova classe de constante para mapear as rotas

---
+++ src/main/java/br/com/tiagoschermack/family_tree/shared/constants/RestConstants.java
@@ -0,0 +1,12 @@
+package br.com.tiagoschermack.family_tree.shared.constants;
+
+public final class RestConstants {
+
+    public static final String PATH_AUTH    = "/auth";
+    public static final String PATH_TREES   = "/trees";
+    public static final String PATH_PERSONS = "/persons";
+    public static final String PATH_EVENTS  = "/events";
+    public static final String PATH_USERS   = "/users";
+
+    private RestConstants() {}
+}
